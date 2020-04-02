c17f80013bb13955509378008302a09187d68428
Eshank Vaish <eshank.vaish@gmail.com>
2020-04-02 20:16:24 +0530

    dblite3 added

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
@@ -84,6 +84,12 @@ DATABASES = {
         'PASSWORD': 'Sandal@123',
         'HOST': 'localhost',
         'PORT': '5432', 
+    }
+}"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
